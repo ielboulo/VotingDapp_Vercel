ad4f6a2787fe062de0bf88124dfb1fc568c6edfb
ielboulo <46560616+ielboulo@users.noreply.github.com>
2023-03-21 09:29:14 +0100

    update

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -25,7 +25,7 @@
  * and Infura PROJECT_ID variables inside.
  * For example, your .env file will have the following structure:
  *
- MNEMONIC = <Your 12 phrase mnemonic>
+ * MNEMONIC = <Your 12 phrase mnemonic>
  * PROJECT_ID = <Your Infura project id>
  *
  * Deployment with Truffle Dashboard (Recommended for best security practice)
@@ -41,15 +41,10 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-require('dotenv').config();
-//const { MNEMONIC, INFURA_ID } = process.env;
-//const MNEMONIC = [process.env.MNEMONIC];
-//const INFURA_ID = [process.env.INFURA_ID];
+// require('dotenv').config();
+// const { MNEMONIC, PROJECT_ID } = process.env;
 
-const MNEMONIC = 'glad cross adjust old spice help include enact hawk turkey battle fiscal';
-const INFURA_ID = '7d6bc4aaada24f17a6f6c66895765ae3';
-
- const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   /**
@@ -70,11 +65,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-/*     development: {
+    development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
-    }, */
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -88,13 +83,13 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-     goerli: {
-       provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${INFURA_ID}`),
-       network_id: 5,       // Goerli's id
-       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-       skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
-     },
+    // goerli: {
+    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
+    //   network_id: 5,       // Goerli's id
+    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // },
     //
     // Useful for private networks
     // private: {
@@ -112,7 +107,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.19",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.18",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
