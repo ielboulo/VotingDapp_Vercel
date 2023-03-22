39eb2eccc12116a2e73ce5d201b96c81de22961c
Ilham EL BOULOUMI <elbouloumi@gmail.com>
2023-03-22 10:30:25 +0100

    update

--- truffle/contracts/Voting.sol
+++ truffle/contracts/Voting.sol
@@ -59,7 +59,7 @@ contract Voting is Ownable {
     */
     constructor(){
         // we assume that the admin has also the right to vote 
-        //voters[msg.sender] = Voter(true, false, 0); // add admin to voters WL at contract deployment
+        //voters[msg.sender] = Voter(true, false, 0);
         voters[msg.sender].isRegistered = true;
         emit VoterRegistered(msg.sender); 
 
